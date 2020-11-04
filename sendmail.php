fb9d5778c2cb54cee911172d320ef6ea743250a5
antizyk <65877663+antizyk@users.noreply.github.com>
2020-11-05 00:46:27 +0100

    Add files via upload

---
+++ sendmail.php
@@ -0,0 +1,72 @@
+<?php
+//!Подключение плагина PHPMailer
+	use PHPMailer\PHPMailer\PHPMailer;
+	use PHPMailer\PHPMailer\Exception;
+
+	require 'phpmailer/src/Exception.php';
+	require 'phpmailer/src/PHPMailer.php';
+//!=================================
+	$mail = new PHPMailer(true);//Объявление плагина
+	$mail->CharSet = 'UTF-8';//Кодировка
+	$mail->setLanguage('ru', 'phpmailer/language/');//Подключаем языковой файл
+	$mail->IsHTML(true);//Включаем возможность HTML тегов в письме
+
+	//От кого письмо
+	$mail->setFrom('указать!', 'Лови письмо от себя');
+	//Кому отправить
+	$mail->addAddress('sashamatvienko0@gmail.com');
+	//Тема письма
+	$mail->Subject = 'Привет! Это я';
+
+	//Рука
+	$hand = "Правая";
+	if($_POST['hand'] == "left"){
+		$hand = "Левая";
+	}
+
+	//Тело письма
+	$body = '<h1>Встречайте супер письмо!</h1>';
+	
+	if(trim(!empty($_POST['name']))){
+		$body.='<p><strong>Имя:</strong> '.$_POST['name'].'</p>';
+	}
+	if(trim(!empty($_POST['email']))){
+		$body.='<p><strong>E-mail:</strong> '.$_POST['email'].'</p>';
+	}
+	if(trim(!empty($_POST['hand']))){
+		$body.='<p><strong>Рука:</strong> '.$hand.'</p>';
+	}
+	if(trim(!empty($_POST['age']))){
+		$body.='<p><strong>Возраст:</strong> '.$_POST['age'].'</p>';
+	}
+	
+	if(trim(!empty($_POST['message']))){
+		$body.='<p><strong>Сообщение:</strong> '.$_POST['message'].'</p>';
+	}
+	
+	//Прикрепить файл
+	if (!empty($_FILES['image']['tmp_name'])) {
+		//путь загрузки файла
+		$filePath = __DIR__ . "/files/" . $_FILES['image']['name']; 
+		//грузим файл
+		if (copy($_FILES['image']['tmp_name'], $filePath)){
+			$fileAttach = $filePath;
+			$body.='<p><strong>Фото в приложении</strong>';
+			$mail->addAttachment($fileAttach);
+		}
+	}
+
+	$mail->Body = $body;
+
+	//Отправляем
+	if (!$mail->send()) {
+		$message = 'Ошибка';
+	} else {
+		$message = 'Данные отправлены!';
+	}
+
+	$response = ['message' => $message];
+
+	header('Content-type: application/json');
+	echo json_encode($response);
+?>
